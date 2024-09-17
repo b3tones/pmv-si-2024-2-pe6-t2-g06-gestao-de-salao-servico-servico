d4b269f6afe17cc170b6ecb1f299ed573ed7aefd
roobe <roobe@ROBERTO>
2024-09-17 19:14:55 -0300

    Adicionar arquivos de projeto.

---
+++ mf-apis-web-services-gestao-de-salao-servicos/WeatherForecast.cs
@@ -0,0 +1,13 @@
+namespace mf_apis_web_services_gestao_de_salao_servicos
+{
+    public class WeatherForecast
+    {
+        public DateOnly Date { get; set; }
+
+        public int TemperatureC { get; set; }
+
+        public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+
+        public string? Summary { get; set; }
+    }
+}
