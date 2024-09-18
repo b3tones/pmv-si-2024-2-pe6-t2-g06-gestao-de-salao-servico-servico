7093a0a6767d069e7808a4c2228b3d7a9aa07cc5
b3tones <rooberto.almeida@hotmail.com>
2024-09-17 21:35:43 -0300

    Criação e Configuração de Serviços

---
+++ mf-apis-web-services-gestao-de-salao-servicos/Models/ServicoCategorias.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace mf_apis_web_services_gestao_de_salao_servicos.Models
+{
+    [Table("ServicoCategorias")]
+    public class ServicoCategoria
+    {
+        [Key]
+        [ScaffoldColumn(false)]
+        public int Id { get; set; }
+
+        [Required]
+        [MaxLength(100)]
+        public string Nome { get; set; }
+
+        public ICollection<ServicoSubCategoria> ServicoSubCategorias { get; set; }
+        public ICollection<Agendamento> Agendamentos { get; set; }
+
+    }
+}
---
+++ mf-apis-web-services-gestao-de-salao-servicos/Models/ServicoSubCategoria.cs
@@ -0,0 +1,31 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace mf_apis_web_services_gestao_de_salao_servicos.Models
+{
+    [Table("ServicoSubCategorias")]
+    public class ServicoSubCategoria
+    {
+        [Key]
+        [ScaffoldColumn(false)]
+        public int Id { get; set; }
+
+        [Required]
+        [MaxLength(100)]
+        public string Nome { get; set; }
+
+        [Required]
+        public TimeSpan Duracao { get; set; }
+
+        [Required]
+        [DisplayFormat(DataFormatString = "{0:N2}")]
+        public decimal Valor { get; set; }
+
+        [Required]
+        public int ServicoCategoriaId { get; set; }
+        public ServicoCategoria ServicoCategoria { get; set; }
+
+        public ICollection<Agendamento> Agendamentos { get; set; }
+    }
+}
