5d3014069ec53f7109c542c80abfe264dc41750d
b3tones <rooberto.almeida@hotmail.com>
2024-09-17 23:50:09 -0300

    Adicionado função GET

---
+++ mf-apis-web-services-gestao-de-salao-servicos/Controllers/ServicosController.cs
@@ -0,0 +1,27 @@
+﻿using mf_apis_web_services_gestao_de_salao_servicos.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace mf_apis_web_services_gestao_de_salao_servicos.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ServicosController : ControllerBase
+    {
+        private readonly AppDbContext _context;
+
+        public ServicosController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult> GetAll()
+        {
+            var model = await _context.ServicoSubCategorias.ToListAsync();
+
+            return Ok(model);
+        }
+    }
+}
