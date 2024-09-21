cb1c05f4365ca1dcc11b4f5c923f6cdd0c928ac9
b3tones <rooberto.almeida@hotmail.com>
2024-09-20 23:24:40 -0300

    CRUD completo funcionando

--- mf-apis-web-services-gestao-de-salao-servicos/Controllers/ServicosController.cs
+++ mf-apis-web-services-gestao-de-salao-servicos/Controllers/ServicosController.cs
@@ -23,5 +23,57 @@ namespace mf_apis_web_services_gestao_de_salao_servicos.Controllers
 
             return Ok(model);
         }
+
+        [HttpPost]
+        public async Task<ActionResult> Create(ServicoCategoria model)
+        {
+            _context.ServicoCategorias.Add(model);
+            await _context.SaveChangesAsync();
+
+            return Ok(model);
+
+
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult> GetById(int id)
+        {
+            var model = await _context.ServicoCategorias
+                .FirstOrDefaultAsync(c=> c.Id == id);
+
+            if (model == null) return NotFound();
+
+            return Ok(model);
+        }
+
+        [HttpPut("{id}")]
+        public async Task<ActionResult> Update(int id, ServicoCategoria model)
+        {
+            if (id != model.Id) return BadRequest();
+
+            var modeloDb = await _context.ServicoCategorias.AsNoTracking()
+                .FirstOrDefaultAsync(c => c.Id == id);
+
+            if (modeloDb == null) return NotFound();
+
+            _context.ServicoCategorias.Update(model);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<ActionResult> Delete(int id) 
+        {
+            var model = await _context.ServicoCategorias.FindAsync(id);
+
+            if (model == null) return NotFound();
+
+            _context.ServicoCategorias.Remove(model);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
     }
 }
